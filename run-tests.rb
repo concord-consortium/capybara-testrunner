6a63882b4c692e4dcf1f0378f0111cb06ee3d42d
Aaron Unger <aunger@concord.org>
2010-08-04 15:58:19 -0400

    Use trollop (http://trollop.rubyforge.org/) for command-line option processing.
    This will let us be a lot more flexible.

--- run-tests.rb
+++ run-tests.rb
@@ -1,35 +1,39 @@
 $:.unshift File.join(File.dirname(__FILE__), 'lib')
 
+require 'trollop'  # For processing command-line options
+
 require 'capybara'
 require 'capybara/dsl'
 require 'transform-results'
 # this is only needed to save a fixture for testing the transform code
 require 'yaml'
 
-Capybara.current_driver = :selenium
+# Process command-line options
+@options = Trollop::options do
+  opt :driver, "Capybara driver", :type => String, :default => "selenium"
+  
+  # look for an environment variable so the port can be changed depending on 
+  # the ci node that is running it
+  opt :sc_server_port, "SC Server Port", :type => :int, :default => (ENV['SC_SERVER_PORT'] || 4020)
+  opt :sc_server_host, "SC Server Host", :type => :string, :default => "localhost"
+  
+  opt :root_dir, "Root directory", :type => :string, :default => ".."
+  opt :tests_dir, "Tests directory", :type => :string, :default => "{apps,frameworks}"
+  opt :exclude_dir, "Exclude test directory", :type => :string, :multi => true
+end
 
-# look for an environment variable so the port can be changed depending on 
-# the ci node that is running it
-sc_server_port = ENV['SC_SERVER_PORT']
-sc_server_port = '4020' if sc_server_port.nil?
-Capybara.app_host = "http://localhost:#{sc_server_port}"
+Capybara.current_driver = @options[:driver].to_sym
+Capybara.app_host = "http://#{@options[:sc_server_host]}:#{@options[:sc_server_port]}"
 
 include Capybara
 
 # find all of the test subfolders in the tests of the current apps
 # FIXME should change to support nested folders inside of tests
-rootDir = '..'
+testFolders = Dir.glob( File.join(@options[:root_dir], @options[:tests_dir], "**", "tests", "*") )
 
-if ARGV[0] then 
-  testFolders = Dir.glob( File.join(rootDir, ARGV[0], "**", "tests", "*") )
-else
-  testFolders = Dir.glob( File.join(rootDir, "{apps,frameworks}","**", "tests", "*") )
-end
-
-
-excludes = [File.join("tmp","**")]
+excludes = [File.join("tmp","**")] + @options[:exclude_dir]
 testFolders = testFolders.reject{|folder|
-    excludes.any?{|exclude| File.fnmatch?(File.join(rootDir, exclude), folder)}
+    excludes.any?{|exclude| File.fnmatch?(File.join(@options[:root_dir], exclude), folder)}
 }
 
 testURLs = testFolders.collect{|folder|
