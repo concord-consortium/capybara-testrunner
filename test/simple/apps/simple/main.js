b3717703b51a69c58f964a1f2b7e3b6d96c2d3ba
Scott Cytacki <scott@concord.org>
2010-10-12 23:52:59 -0400

    a simple app to test this project
    This was generated with 'sc-init simple', and then a single file simple/apps/simple/tests/top_level.js was added with one always passing test.

---
+++ test/simple/apps/simple/main.js
@@ -0,0 +1,30 @@
+// ==========================================================================
+// Project:   Simple
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Simple */
+
+// This is the function that will start your app running.  The default
+// implementation will load any fixtures you have created then instantiate
+// your controllers and awake the elements on your page.
+//
+// As you develop your application you will probably want to override this.
+// See comments for some pointers on what to do next.
+//
+Simple.main = function main() {
+
+  // Step 1: Instantiate Your Views
+  // The default code here will make the mainPane for your application visible
+  // on screen.  If you app gets any level of complexity, you will probably 
+  // create multiple pages and panes.  
+  Simple.getPath('mainPage.mainPane').append() ;
+
+  // Step 2. Set the content property on your primary controller.
+  // This will make your app come alive!
+
+  // TODO: Set the content property on your primary controller
+  // ex: Simple.contactsController.set('content',Simple.contacts);
+
+} ;
+
+function main() { Simple.main(); }
---
+++ test/simple/apps/simple/tests/top_level.js
@@ -0,0 +1,7 @@
+/*globals module test ok equals same stop start */
+
+module("Simple.TopLevel");
+
+test("Test js files in the root of tests folder are processed by sc-test", function() {
+  ok(true, "This test ran");
+});
