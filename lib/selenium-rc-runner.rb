0f54e18022bc065e7b3d36228dabe60d159d158a
Scott Cytacki <scott@concord.org>
2010-10-23 00:09:41 -0400

    changed the json encoding so it will work on IE

--- lib/selenium-rc-runner.rb
+++ lib/selenium-rc-runner.rb
@@ -18,7 +18,7 @@ class SeleniumRCRunner
   end
   
   def js_eval(javascript)
-    results_str = @browser.js_eval("JSON.stringify(window.#{javascript})")
+    results_str = @browser.js_eval("SC.json.encode(window.#{javascript})")
     JSON.parse(results_str)
   end
   
