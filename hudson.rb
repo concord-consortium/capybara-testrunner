ee3ba8e51138eca3eac04c9590d60d083c53b168
Scott Cytacki <scott@concord.org>
2010-10-25 16:01:21 -0400

    turned off image capture by default in hudson.rb

--- hudson.rb
+++ hudson.rb
@@ -15,6 +15,7 @@ require 'trollop'  # For processing command-line options
   opt :sc_server_path, "Path to sc-server command", :short => 'p', :type => :string, :default => "sc-server"
   opt :sc_server_host, "SC Server Host", :short => 's', :type => :string, :default => "localhost"
   opt :runner, "Browser runner", :short => 'b', :type => String, :default => "capybara"
+  opt :image, "Save a png snapshot of the test page(doesn't work in all browser runners)", :short => 'i', :default => false
 end
 
 # NOTE the sc_server_host is not used when starting up the sc-server it is only used by the browser runner
@@ -32,11 +33,13 @@ sc_server.start
 run_tests_cmd = ["ruby -rubygems '#{File.join(File.dirname(__FILE__), 'run-tests.rb')}'",
                  "-p #{port}",
                  "-r .", # set the root for looking for the tests to be this directory
-                 "-i -h",
+                 "#{'-i' if @options[:image]}",
+                 "-h",
                  "-t #{@options[:tests_dir]}",
                  "-o #{@options[:results_dir]}",
                  "-s #{@options[:sc_server_host]}",
                  "-b #{@options[:runner]}"]
+puts run_tests_cmd.join(' ')
 system(run_tests_cmd.join(' '))
 run_test_result = $?
 sc_server.stop
